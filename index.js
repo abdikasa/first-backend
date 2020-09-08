fa05e0596e65f79c89b1e154d32f84c24521ca6a
abdikasa <abdikasa@sheridancollege.ca>
2020-09-07 22:11:09 -0400

    Adding express to our application with endpoints

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 
 let notes = [
   {
@@ -21,11 +22,18 @@ let notes = [
   },
 ];
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(JSON.stringify(notes));
+//on root, show this
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+//specifies a certain path and shows the notes data.
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
 });
 
 const PORT = 3002;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT),
+  () => {
+    console.log(`Server running on port ${PORT}`);
+  };
